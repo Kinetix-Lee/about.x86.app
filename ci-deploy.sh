ead3cdcee68b8101829c8ff71bb51df42b921813
Kinetix <x86app@126.com>
2020-10-03 11:54:10 +0800

    添加 SSH 密钥以自动部署到 GitHub

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -2,9 +2,21 @@
 
 export COMMIT_ID=$(git rev-parse --short HEAD)
 
+# start SSH agent
+eval `ssh-agent -s`
+ssh-add ~/github.key
+
+# Git configurations
+git config user.name "Kinetix-Lee"
+git config user.email "lee@x86.app"
+
+
 # fetch repo
 git clone --depth=1 git@github.com:Kinetix-Lee/Kinetix-Lee.git deploy
 cd deploy
+
+# clear ssh-agent processes
+killall ssh-agent -u deploy
 
 # apply changes
 rm -rf pages
